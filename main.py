c5a18f1f2693e8d04d2a735d78b6dab5211d202a
Meet Patel <patelmeet59@gmail.com>
2021-05-29 00:17:50 +0530

    feat: add error handling in YT-Transcript - create README - create .prettierrc
    Signed-off-by: Meet Patel <patelmeet59@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi, TooManyRequests, CouldNotRetrieveTranscript, TranscriptsDisabled
 
 # run server:
 # export FLASK_APP=main.py && flask run --host 0.0.0.0
@@ -15,14 +15,27 @@ def check():
         'response': '200 Success'
     }
 
+
 @app.route('/getcaptions', methods=['POST'])
 def getcaptions():
     request_data = request.get_json()
     yt_id = request_data['yt_id']
-    result = YouTubeTranscriptApi.get_transcript(yt_id, languages=[ 'en'])
+    try:
+        result = YouTubeTranscriptApi.get_transcript(yt_id, languages=['en'])
+    except TooManyRequests:
+        result = "TooManyRequests"
+        print(result)
+    except TranscriptsDisabled:
+        result = "TranscriptsDisabled"
+        print(result)
+    except CouldNotRetrieveTranscript:
+        result = "CouldNotRetrieveTranscript"
+        print(result)
+
     return {
         'response': result
     }
+
 
 if __name__ == '__main__':
     # app.run()
